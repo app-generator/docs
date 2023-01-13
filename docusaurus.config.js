5a462bb9960a62975c628776412ea0532a1e70e0
App Generator <51070104+app-generator@users.noreply.github.com>
2023-01-13 12:20:00 +0200

    Site is LIVE

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
 					sidebarPath: require.resolve("./sidebars.js"),
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/app-generator/docs-v2/tree/main/",
+					editUrl: "https://github.com/app-generator/docs/tree/main/",
 				},
 				blog: false,
 				// blog: {
